7d7733587d8474c9dd0ea21c28ac8d43575400ee
Ramy Al Zuhouri <ramy@klikkie.com>
2022-11-29 11:23:33 +0100

    Project setup

---
+++ Venues/VenuesApp.swift
@@ -0,0 +1,17 @@
+//
+//  VenuesApp.swift
+//  Venues
+//
+//  Created by Ramy Al Zuhouri on 29/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct VenuesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
